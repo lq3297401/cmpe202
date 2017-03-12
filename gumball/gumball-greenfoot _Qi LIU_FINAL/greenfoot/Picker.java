fe799f563970eee64419ce0e8725da7d9b56f393
lq3297401 <liuqi0087@gmail.com>
2017-03-11 18:41:23 -0800

    Add files via upload

---
+++ gumball/gumball-greenfoot _Qi LIU_FINAL/greenfoot/Picker.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+/**
+ * Write a description of class Picker here.
+ * CMPE 202, original file from professor Paul
+ * @author Qi LIU 
+ * @version 3/9/2017
+ */
+public class Picker extends Alien
+{
+    /**
+     * Act - do whatever the Picker wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    Message m = new Message();
+    
+    public void pick(){
+        
+    }
+    
+    public void moveTo(int x, int y){
+        setLocation(x,y);
+    }
+    
+    /**
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    */
+}
